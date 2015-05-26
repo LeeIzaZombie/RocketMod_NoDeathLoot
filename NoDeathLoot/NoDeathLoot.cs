33c49e60a4c0cc3ce2841ca289f24a74b9535cbd
LeeIzaZombie <leeizazombie@outlook.com>
2015-05-26 19:26:07 +0100

    Publishing project.

---
+++ NoDeathLoot/NoDeathLoot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using Rocket.Unturned;
+using Rocket.Unturned.Plugins;
+
+namespace NoDeathLoot
+{
+    public class PluginNoDeathLoot : RocketPlugin
+    {
+        public bool RunFromConsole
+        {
+            get { return true; }
+        }
+        public string Name
+        {
+            get { return "nodeathloot"; }
+        }
+        protected override void Load()
+        {
+            Rocket.Unturned.Events.RocketPlayerEvents.OnPlayerDeath += RocketPlayerEvents_OnPlayerDeath;
+        }
+
+        private void RocketPlayerEvents_OnPlayerDeath(Rocket.Unturned.Player.RocketPlayer player, SDG.EDeathCause cause, SDG.ELimb limb, Steamworks.CSteamID murderer)
+        {
+            player.Inventory.Clear(); //That's all.. ikr but it works!
+        }
+
+    }
+}
